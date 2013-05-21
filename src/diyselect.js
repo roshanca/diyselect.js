fbab97698a2f74a968453615a78d63e18d249842
RoshanWu <wwj1983@gmail.com>
2013-05-21 17:37:25 +0800

    主要修改：1、去除选中后的选项不在列表显示的方法；2、出现滚动条时，键盘操作可控制滚动

--- src/diyselect.js
+++ src/diyselect.js
@@ -1,7 +1,7 @@
 /**
  * 用其它 HTMLElement 模拟 select
  * 属性：UI组件
- * 版本: 0.9.1
+ * 版本: 0.9.3
  * 依赖：jQuery ~> 1.7.2, pin ~> 0.8.2
  * 开发维护：wuwj
  */
@@ -10,12 +10,6 @@ var DiySelect = function (select, options) {
   options = $.extend({}, $.fn.diySelect.defaults, options);
 
   this.options = options;
-  this.selectSpan = $('<span/>').addClass(options.selectClass);
-  this.optionUl = $('<ul/>').addClass(options.optionClass);
-  this.option = $(select).find('option');
-  this.optionSize = this.option.length;
-  this.arrowHtml = '<i class="' + this.options.arrowClass + '"></i>';
-  this.bgIframe = $('<iframe/>').addClass('bgiframe').attr('frameborder', '0');
   this.isInit = false;
 
   this.init(select);
@@ -31,8 +25,13 @@ DiySelect.prototype = {
       return;
     }
 
-    var that = this;
-    var active = -1;
+    this.selectSpan = $('<span/>').addClass(this.options.selectClass);
+    this.optionDiv = $('<div/>').addClass(this.options.optionClass);
+    this.optionUl = $('<ul/>');
+    this.option = $(select).find('option');
+    this.optionSize = this.option.length;
+    this.arrowHtml = '<i class="' + this.options.arrowClass + '"></i>';
+    this.bgIframe = $('<iframe/>').addClass('bgiframe').attr('frameborder', '0');
 
     // 组装模拟元件
     this.setupOption();
@@ -42,42 +41,45 @@ DiySelect.prototype = {
     // 隐藏原 `select`，再其后加入模拟的 `HTMLElement`
     $(select).hide();
     this.selectSpan.insertAfter($(select));
-    this.optionUl.insertAfter(this.selectSpan).hide();
+    this.optionDiv.insertAfter(this.selectSpan).hide();
 
+    // 初始化完成标记
     this.isInit = true;
+
+    var that = this;
+    var list = this.optionDiv.find('li');
+    var active = -1;
 
     // 事件绑定
     this.selectSpan.on('click', function (e) { 
       e.preventDefault();
       e.stopPropagation();
-      $('.' + that.options.optionClass).hide(); 
+      $('.' + that.options.optionClass).hide();
 
-      if (that.optionUlVisible) {
+      if (that.optionVisible) {
         that.hideOption();
       } else {
         that.showOption();
       }
     });
 
-    var list = this.optionUl.find('li');
-
     list.on('click', function (e) {
-      var value = $(this).text();
+      var text = $(this).text();
 
       e.preventDefault();
       e.stopPropagation();
-      that.chooseOption(value);
+      that.chooseOption(text);
     });
 
     list.on('mouseenter', function () {
       list.removeClass('active');
       $(this).addClass('active');
-      active = list.index(this);
+      active = list.filter(':visible').index(this);
     });
 
-    $(select).on('choose', function (e, value) {
+    $(select).on('choose', function (e, text) {
       e.stopPropagation();
-      that.chooseOption(value);
+      that.chooseOption(text);
     });
 
     // 窗口变化时调整位置
@@ -85,20 +87,20 @@ DiySelect.prototype = {
 
     // 模拟 `select` 失焦
     $(document).on('click', function () {
-      if (that.optionUl.is(':visible')) { that.hideOption(); }
+      if (that.optionDiv.is(':visible')) { that.hideOption(); }
     });
 
     // 键盘事件
     $(document).on('keydown', function (e) {
-      if (that.optionUl.is(':visible')) {
+      if (that.optionDiv.is(':visible')) {
         switch (e.keyCode) {
           case 13: // enter
-            that.hideOption();
-
             if (active !== -1) {
-              var value = list.slice(active, active + 1).text();
-              that.chooseOption(value);
+              var text = list.slice(active, active + 1).text();
+              that.chooseOption(text);
             }
+
+            that.hideOption();
             break;
           case 27: // esc
             that.hideOption();
@@ -110,7 +112,6 @@ DiySelect.prototype = {
           case 40: // down
             e.preventDefault();
             moveSelect(1);
-
             break;
         }
       }
@@ -121,7 +122,7 @@ DiySelect.prototype = {
      * @param  {float} step 位移步长
      */
     function moveSelect(step) {
-      var count = that.optionSize;
+      var count = list.length;
 
       active += step;
 
@@ -132,14 +133,13 @@ DiySelect.prototype = {
       } else {
         list.removeClass('active');
         list.slice(active, active + 1).addClass('active');
+
+        // 出现滚动条的情况
+        if ( active >= (that.options.maxSize / 2) && count > that.options.maxSize) {
+          that.optionDiv.scrollTop(list.height() * (active - (that.options.maxSize / 2)));
+        }
       }
     }
-  },
-
-  setupSelect: function () {
-    var text = this.options.checkText || this.option.filter(":selected").text();
-
-    this.chooseOption(text);
   },
 
   setupOption: function () {
@@ -159,70 +159,74 @@ DiySelect.prototype = {
       fragment.appendChild(li);
     }
 
+    this.optionDiv.append(this.optionUl);
     this.optionUl.get(0).appendChild(fragment);
+  },
+
+  setupSelect: function () {
+    var checkText = this.options.checkText || this.option.filter(':selected').text();
+
+    // 初始选择（可设置）
+    this.chooseOption(checkText);
   },
 
   setPosition: function () {
     // 可视窗口顶部到 `selectSpan` 的距离
     var top_height = this.selectSpan.offset().top + this.selectSpan.outerHeight() - $('body').scrollTop();
 
-    // 可视窗口剩余空间与 `optionUl` 高度的差值
-    var diff = $(window).height() - top_height - this.optionUl.height();
+    // 可视窗口剩余空间与 `optionDiv` 高度的差值
+    var diff = $(window).height() - top_height - this.optionDiv.height();
 
-    // 差值大于零，说明剩余空间还可容纳 `optionUl`
-    // `optionUl` 就位居 `selectSpan` 正下方展示
+    // 差值大于零，说明剩余空间还可容纳 `optionDiv`
+    // `optionDiv` 就位居 `selectSpan` 正下方展示
     // 反之亦然
     if ( diff > 0 ) {
-      this.optionUl.pin({
+      this.optionDiv.pin({
         base: this.selectSpan,
         baseXY: [0, '100%-1px']
       });
     } else {
-      this.optionUl.pin({
+      this.optionDiv.pin({
         base: this.selectSpan,
         selfXY: [0, '100%-1px']
       });
     }
   },
 
+  chooseOption: function(text) {
+    var list = this.optionUl.find('li');
+
+    this.hideOption();
+    this.selectSpan.html(text + this.arrowHtml);
+    this.option.attr('selected', false);
+    
+    for (var i = 0; i < this.optionSize; i++) {
+      if (text === this.option[i].text) {
+        // 原生 `select` 跟随选择
+        this.option.eq(i).attr('selected', true);
+        list.data('selectedIndex') = i;
+      }
+    }
+  },
+
   showOption: function () {
-    this.optionUl.show();
+    this.optionDiv.show();
 
-    this.optionUl.height(Math.min(this.optionUl.height(), 
-      this.optionUl.find('li').height() * this.options.maxSize));
+    this.optionDiv.height(Math.min(this.optionDiv.height(), 
+      this.optionDiv.find('li').height() * this.options.maxSize));
 
-    this.optionUl.css({
+    this.optionDiv.css({
       'min-width': this.selectSpan.outerWidth(),
       'z-index': this.options.zIndex
     });
 
     this.setPosition();
-    this.optionUlVisible = true;
+    this.optionVisible = true;
   },
 
   hideOption: function () {
-    this.optionUl.hide();
-    this.optionUlVisible = false;
-  },
-
-  chooseOption: function (value) {
-    var list = this.optionUl.find('li');
-
-    this.hideOption();
-    this.selectSpan.html(value + this.arrowHtml);
-    this.option.attr('selected', false);
-    
-    for (var i = 0; i < this.optionSize; i++) {
-      if (value === this.option[i].text) {
-        // 原生 `select` 中跟随选择
-        this.option.eq(i).attr('selected', true);
-        list.selectedIndex = i;
-      }
-    }
-  
-    // 已被选中的选项不在列表中展示
-    list.show();
-    list.eq(list.selectedIndex).hide();
+    this.optionDiv.hide();
+    this.optionVisible = false;
   }
 };
 
